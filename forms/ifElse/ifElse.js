2ee97456b69ac874609d4db677243396652e8959
Makala Smith <mms96354@creighton.edu>
2021-02-28 15:13:20 -0600

    created firstName, userState, and userTemperature variables

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,1 +1,3 @@
-let firstName = 
+let firstName = prompt("What is your first name?")
+let usersState = prompt("What state do you live in? (Ex: NE)")
+let usersTemperature = prompt("What is the current outside temperature? (Ex: 55)")
