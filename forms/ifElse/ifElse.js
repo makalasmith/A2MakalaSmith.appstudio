cbdd34abab2108fb8551c87029e31f72eb5b679b
Makala Smith <mms96354@creighton.edu>
2021-02-28 15:46:39 -0600

    inserted the if else statement

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,3 +1,4 @@
+/*
 //get the users first name
 let firstName = prompt("What is your first name?")
 console.log(firstName)
@@ -18,5 +19,12 @@ console.log(array)
 let messages =["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
 
 //if else statement about the users state and weather
-if (usersState = "NE" && usersTemperature >= 32)
-  console.log(message[0])
+if (usersState = "NE" && usersTemperature < 32)
+  console.log(messages[0])
+else if (usersState = "NE" && usersTemperature >= 32 && usersTemperature <=50)
+  console.log(messages[1])
+else if (usersState = "FL" && usersTemperature >= 32 && usersTemperature <=50)
+  console.log(messages[2])
+else if (usersState = "FL" && usersTemperature >= 50 && usersTemperature <=70)
+  console.log(messages[3])
+*/
